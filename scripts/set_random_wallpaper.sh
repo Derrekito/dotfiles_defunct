02b8be2c7b0d49d767a762e01619ab281eae4c1a
Derrek Landauer <Derrekito@users.noreply.github.com>
2024-01-15 15:43:41 -0700

    Added 43:18 support to wallpaper script

--- scripts/set_random_wallpaper.sh
+++ scripts/set_random_wallpaper.sh
@@ -2,19 +2,36 @@
 
 WALLPAPER_PATH="$HOME/wallpaper"
 
-# Get the resolution of the active display
-RESOLUTION=$(xrandr --current | grep '*' | uniq | awk '{print $1}')
-WIDTH=$(echo $RESOLUTION | cut -d 'x' -f1)
-HEIGHT=$(echo $RESOLUTION | cut -d 'x' -f2)
+function get_current_ar()
+{
+    # Get the resolution of the active display
+    RESOLUTION=$(xrandr --current | grep '*' | uniq | awk '{print $1}')
+    WIDTH=$(echo $RESOLUTION | cut -d 'x' -f1)
+    HEIGHT=$(echo $RESOLUTION | cut -d 'x' -f2)
 
-# Calculate aspect ratio
-ASPECT_RATIO=$(echo "scale=2; $WIDTH/$HEIGHT" | bc)
+    # Calculate aspect ratio
+    echo "scale=2; $WIDTH/$HEIGHT" | bc
+}
+
+function check_ar()
+{
+    local AR=$1
+    echo "$ASPECT_RATIO == $AR" | bc -l
+}
+
+ASPECT_RATIO=$(get_current_ar)
+
+ASPECT_RATIO_16_9=$(echo "scale=2; 16/9" | bc)
+ASPECT_RATIO_21_9=$(echo "scale=2; 21/9" | bc)
+ASPECT_RATIO_43_18=$(echo "scale=2; 43/18" | bc)
 
 # Set the wallpaper directory based on aspect ratio
-if (( $(echo "$ASPECT_RATIO == 1.77" | bc -l) )); then
+if (( $(check_ar $ASPECT_RATIO_16_9) )); then
     WALLPAPER_DIR="$WALLPAPER_PATH/16:9"
-else
+elif (( $(check_ar $ASPECT_RATIO_21_9) )); then
     WALLPAPER_DIR="$WALLPAPER_PATH/21:9"
+else
+    WALLPAPER_DIR="$WALLPAPER_PATH/43:18"
 fi
 
 # Select a random wallpaper from the appropriate directory
